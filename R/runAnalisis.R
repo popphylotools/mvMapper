b7cdc78397fc8474943782622f5268d6dff11532
Woods26 <forest.bremer@gmail.com>
2016-12-29 08:05:25 -1000

    remove adegenet (no more popgen)

--- R/runAnalisis.R
+++
@@ -1,7 +0,0 @@
-library(adegenet)
-
-Rosenberg <- read.structure("Rosenberg_783msats.str", n.ind=1048, n.loc=783,  onerowperind=F, col.lab=1, col.pop=2, col.others=c(3:5), row.marknames=NULL, NA.char="-9", ask=FALSE, quiet=FALSE)
-
-dapc1 <-dapc(Rosenberg, n.pca=20, n.da=200)
-
-saveRDS(dapc1, file="./data/dapc.rds")
