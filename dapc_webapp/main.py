c5e9ffc7600d6085690edc173a367de1f1057d23
Woods26 <forest.bremer@gmail.com>
2017-03-24 08:35:15 -1000

    corrected gcd command

--- dapc_webapp/main.py
+++ dapc_webapp/main.py
@@ -18,7 +18,7 @@ SIZES = list(range(6, 22, 3))
 
 def get_data():
     """Read data from csv and transform map coordinates. """
-    print(os.path.getcwd())
+    print(os.getcwd())
     data = pd.read_csv("/data/webapp_data.csv")
 
     data['grp'] = data['grp'].apply(str)
