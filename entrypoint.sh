2ea017b8f1f0ef5932b98c3ebb04008ac0722917
Woods26 <forest.bremer@gmail.com>
2017-05-09 15:00:55 -1000

    helps if you look for your config file in the right place

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # if we have mvmapper_input.csv, run webapp
-if [ -e "data/mvmapper_input.csv" ] && [ -e "data/config.toml" ]
+if [ -e "data/mvmapper_input.csv" ] && [ -e "config/config.toml" ]
 then
     bokeh serve /bokeh/dapc_webapp/ --host ${APP_URL}:${APP_PORT} --port 5006
 fi
