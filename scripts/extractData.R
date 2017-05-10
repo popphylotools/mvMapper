1616be5a9233798623c08b251ece010484fe551b
Woods26 <forest.bremer@gmail.com>
2017-05-09 14:55:33 -1000

    cleaned up data files and scripts as data prep has been externalized
    fixes #44

--- scripts/extractData.R
+++
@@ -1,3 +0,0 @@
-dapc1 <- readRDS("./data/dapc.rds")
-
-for (name in names(dapc1)[is.element(names(dapc1), list("assign", "eig", "ind.coord", "posterior", "grp"))]) { write.csv(dapc1[name], file = paste("./data/", name, ".csv", sep=""), na="") }
