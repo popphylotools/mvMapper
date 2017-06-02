ffb2664e6ea68b7eb81b71a7fac295a8d8f96006
Woods26 <forest.bremer@gmail.com>
2017-06-02 09:26:53 -1000

    changed import names to aid comprehension

--- mvMapper/main.py
+++ mvMapper/main.py
@@ -3,10 +3,10 @@ from jinja2 import Environment, FileSystemLoader
 from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler
 
-from bokeh.application import Application
-from bokeh.application.handlers import FunctionHandler
-from bokeh.embed import autoload_server
-from bokeh.server.server import Server
+from bokeh.application import Application as bkApplication
+from bokeh.application.handlers import FunctionHandler as bkFunctionHandler
+from bokeh.embed import autoload_server as bk_autoload_server
+from bokeh.server.server import Server as bkServer
 
 from app import modify_doc
 
@@ -19,15 +19,15 @@ class IndexHandler(RequestHandler):
 
     def get(self):
         template = env.get_template('embed.html')
-        script = autoload_server(model=None, url='http://localhost:5006/bkapp')
+        script = bk_autoload_server(model=None, url='http://localhost:5006/bkapp')
         self.write(template.render(script=script, template="Tornado"))
 
 
-bokeh_app = Application(FunctionHandler(modify_doc))
+bokeh_app = bkApplication(bkFunctionHandler(modify_doc))
 
 io_loop = IOLoop.current()
 
-server = Server({'/bkapp': bokeh_app}, io_loop=io_loop, extra_patterns=[('/', IndexHandler)])
+server = bkServer({'/bkapp': bokeh_app}, io_loop=io_loop, extra_patterns=[('/', IndexHandler)])
 server.start()
 
 if __name__ == '__main__':
