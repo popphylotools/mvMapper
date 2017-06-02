5686c5491d97bb5c9869933d90c5599eba1ae11a
Woods26 <forest.bremer@gmail.com>
2017-06-01 15:12:26 -1000

    entrypoint.sh corrected

--- mvMapper/entrypoint.sh
+++ mvMapper/entrypoint.sh
@@ -3,5 +3,5 @@
 # if we have mvmapper_input.csv, run webapp
 if [ -e "data/mvmapper_input.csv" ] && [ -e "config/config.toml" ]
 then
-    bokeh serve /bokeh/dapc_webapp/ --host ${APP_URL}:${APP_PORT} --port 5006
+    python main.py
 fi
