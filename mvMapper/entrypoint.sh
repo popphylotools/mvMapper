779759842a0bea3051e7a6215d5ac417774c020e
Woods26 <forest.bremer@gmail.com>
2017-06-09 10:28:37 -1000

    okay, should be the last bit

--- mvMapper/entrypoint.sh
+++ mvMapper/entrypoint.sh
@@ -6,5 +6,5 @@ mkdir -p data
 # if we have mvmapper_input.csv, and config.toml, run webapp
 if [ -e "exampleData/mvmapper_input.csv" ] && [ -e "config/config.toml" ]
 then
-    python main.py ${APP_URL} ${APP_PORT}
+    python main.py ${APP_URL}:${APP_PORT} 5006
 fi
