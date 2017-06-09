fd0039108e40a0e365d9b8c182dd59e88d09a2bb
Woods26 <forest.bremer@gmail.com>
2017-06-08 17:56:21 -1000

    fixed check in entrypoint.sh so it actually runs the server -_-

--- mvMapper/entrypoint.sh
+++ mvMapper/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # if we have mvmapper_input.csv, run webapp
-if [ -e "data/mvmapper_input.csv" ] && [ -e "config/config.toml" ]
+if [ -e "exampleData/mvmapper_input.csv" ] && [ -e "config/config.toml" ]
 then
     python main.py
 fi
