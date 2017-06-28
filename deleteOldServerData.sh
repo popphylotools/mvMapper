4a41ed5cee3e0d18213a9a57b1e438d5d11cce5d
Woods26 <forest.bremer@gmail.com>
2017-06-27 16:39:05 -1000

    corrected data path in cron script

--- deleteOldServerData.sh
+++ deleteOldServerData.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ "$DAYS_TO_KEEP_DATA" != "0" ]; then
-    find /root/server/data -type f -not -iname "*.*" -mtime +${DAYS_TO_KEEP_DATA} -delete
+    find /webapp/data -type f -not -iname "*.*" -mtime +${DAYS_TO_KEEP_DATA} -delete
 fi
