e54f23150f45e7a0c84a1de386165b4ce2e73b5d
Forest Bremer <woods26@users.noreply.github.com>
2017-10-25 12:46:46 -1000

    implemented check at upload time to insure lat/lon in range

--- webapp/main.py
+++ webapp/main.py
@@ -146,7 +146,12 @@ class POSTHandler(tornado.web.RequestHandler):
                 response_to_send["newUuid"] = new_filename
 
                 # validation
-                if ".csv" in filename:
+                # check if .csv extension
+                if ".csv" not in filename:
+                    response_to_send["success"] = False
+                    response_to_send["error"] = 'Only .csv extension allowed.'
+                else:
+                    # check if parses with pandas
                     try:
                         df = pandas.read_csv(io.BytesIO(body))
                     except Exception as e:
@@ -155,15 +160,27 @@ class POSTHandler(tornado.web.RequestHandler):
                         log.error(str(e))
                     else:
                         columns = set(df.columns)
-                        if {"key", "lat", "lon"}.issubset(columns):
-                            df.to_csv("data/" + new_filename, header=True, index=False)
-                            response_to_send["success"] = True
-                        else:
+                        # check if has needed columns
+                        if not {"key", "lat", "lon"}.issubset(columns):
                             response_to_send["success"] = False
                             response_to_send["error"] = 'Ensure that "key", "lat", and "lon" columns exist.'
-                else:
-                    response_to_send["success"] = False
-                    response_to_send["error"] = 'Only .csv extension allowed.'
+                        else:
+                            # check if lat/lon in range
+                            max_lon = df['lon'].max()
+                            min_lon = df['lon'].min()
+                            max_lat = df['lat'].max()
+                            min_lat = df['lat'].min()
+                            if not ((-180 <= max_lon <= 180)
+                               and  (-180 <= min_lon <= 180)
+                               and  (-90  <= max_lat <= 90)
+                               and  (-90  <= min_lat <= 90)):
+                                response_to_send["success"] = False
+                                response_to_send["error"] = ('Ensure that "lat" in range -90 to 90, '
+                                                             'and "lon" in range -180 to 180.')
+                            else:
+                                # passed all tests
+                                df.to_csv("data/" + new_filename, header=True, index=False)
+                                response_to_send["success"] = True
 
         log.info(json.dumps(response_to_send))
         self.write(json.dumps(response_to_send))
